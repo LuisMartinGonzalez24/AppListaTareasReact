b65955a62ba54beff4aecc62ef81c5175c4bb084
Luis Martin <luismartingt3330@hotmail.com>
2021-06-16 18:02:27 -0400

    Subiendo primeros archivos de la app de tareas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Header from './Components/header.js';
+import FormularioTareas from './Components/FormularioTareas';
+import ListaTareas from './Components/ListaTareas';
 
-function App() {
+const App = () => {
+
+  const tareasGuardadas = 
+    localStorage.getItem('tareas') ? 
+    JSON.parse(localStorage.getItem('tareas')) 
+    : [];
+  
+  const [tareas, cambiarTareas] = useState(tareasGuardadas);
+
+  useEffect(() => {
+      localStorage.setItem('tareas', JSON.stringify(tareas));
+  }, [tareas]);
+
+  let configMostrarCompletadas = '';
+  
+  if (localStorage.getItem('mostrarCompletadas') === null) {
+      configMostrarCompletadas = true;
+  }else{
+    configMostrarCompletadas = localStorage.getItem('mostrarCompletadas') === 'true';
+  }
+
+  const [mostrarCompletadas, cambiarMostrarCompletadas] = useState(configMostrarCompletadas);
+
+  useEffect(() => {
+    localStorage.setItem('mostrarCompletadas', mostrarCompletadas.toString());
+  }, [mostrarCompletadas]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "contenedor">
+      <Header mostrarCompletadas = {mostrarCompletadas} cambiarMostrarCompletadas = {cambiarMostrarCompletadas}/>
+      <FormularioTareas tareas = {tareas} cambiarTareas = {cambiarTareas}/>
+      <ListaTareas tareas = {tareas} cambiarTareas = {cambiarTareas} mostrarCompletadas = {mostrarCompletadas} />
     </div>
   );
 }
-
+ 
 export default App;
